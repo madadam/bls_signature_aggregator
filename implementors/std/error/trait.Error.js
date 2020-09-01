785361f19e59268f71dda8b29b7913f014b70bb5
S-Coyle <S-Coyle@users.noreply.github.com>
2020-09-01 15:16:21 +0000

    deploy: 556d14767c190907aeee84be4138e0926850d43c

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["bls_signature_aggregator"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"bls_signature_aggregator/signature_aggregator/enum.AccumulationError.html\" title=\"enum bls_signature_aggregator::signature_aggregator::AccumulationError\">AccumulationError</a>","synthetic":false,"types":["bls_signature_aggregator::signature_aggregator::AccumulationError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["bls_signature_aggregator"] = {"name":"","files":["lib.rs","proof.rs","signature_aggregator.rs"]};
+createSourceSidebar();
